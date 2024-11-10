18738a3ce6d377bf070f3f1e7fa932f858f010b5
Julio Matias <juliocesark2@hotmail.com>
2024-11-10 20:33:02 +0000

    feat: implement logo

---
+++ frontend/src/components/Logo/Logo.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles';
+
+export const Logo = () => {
+  return (
+    <div>
+      <S.Title>UOU</S.Title>
+    </div>
+  )
+}
---
+++ frontend/src/components/Logo/styles.ts
@@ -0,0 +1,7 @@
+import tw from 'tailwind-styled-components';
+
+export const Title = tw.h1`
+  text-2xl 
+  font-bold 
+  text-white
+`
