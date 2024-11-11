4689d69f91cbf310a72ce2d204b8f2aafd5b332a
Julio Matias <juliocesark2@hotmail.com>
2024-11-11 18:04:22 +0000

    fix: erros linter and added textColor

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,10 +1,10 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   safelist: [
     {
@@ -28,8 +28,12 @@ const config: Config = {
       },
       colors: {
         primary: '#201f27',
-        secundary: '#2E3038',
+        secondary: '#2E3038',
         link: '#0077B6'
+      },
+      textColor: {
+        primary: '#4b5563',
+        secondary: '#9ca3af'
       }
     }
   },
