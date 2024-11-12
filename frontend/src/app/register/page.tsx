917d0cca0929f463e8df2efcf2066d1ac7a31bd8
Julio Matias <juliocesark2@hotmail.com>
2024-11-12 04:16:52 +0000

    feat: implement route register

---
+++ frontend/src/app/register/page.tsx
@@ -0,0 +1,12 @@
+import { HeaderPage } from '@/components/HeaderPage';
+
+export default function Register() {
+  return (
+    <main>
+      <HeaderPage
+        tittle="Novo usuário"
+        description="Informe os campos a seguir para criar novo usuário."
+      />
+    </main>
+  );
+}
