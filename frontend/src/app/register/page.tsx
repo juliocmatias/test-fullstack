083d4ef1dcca647cefa3af08a4cd9fe4045273f7
Julio Matias <juliocesark2@hotmail.com>
2024-11-13 05:10:05 +0000

    feat: implement form register client

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -1,4 +1,8 @@
+import { ButtonPrimary } from '@/components/ButtonPrimary';
+import { ButtonSecondary } from '@/components/ButtonSecondary';
 import { HeaderPage } from '@/components/HeaderPage';
+import { Input } from '@/components/Input';
+import { Select } from '@/components/Select';
 
 export default function Register() {
   return (
@@ -7,6 +11,29 @@ export default function Register() {
         tittle="Novo usuário"
         description="Informe os campos a seguir para criar novo usuário."
       />
+      <div>
+        <form className="mb-5">
+          <div className="my-5">
+            <Input label="Nome" placeholder="Informe seu nome" />
+            <Input label="E-mail" placeholder="Informe seu e-mail" />
+            <Input label="CPF" placeholder="Informe seu CPF" />
+            <Input label="Telefone" placeholder="Informe seu telefone" />
+            <Select
+              label="Status"
+              options={[
+                { label: 'Ativo', value: 'ativo' },
+                { label: 'Inativo', value: 'inativo' },
+                { label: 'Aguardando ativação', value: 'aguardando_ativacao' },
+                { label: 'Desativado', value: 'desativado' }
+              ]}
+            />
+          </div>
+          <div className="flex gap-8">
+            <ButtonPrimary type="submit">Criar</ButtonPrimary>
+            <ButtonSecondary type="button">Voltar</ButtonSecondary>
+          </div>
+        </form>
+      </div>
     </main>
   );
 }
