ab06871e20ee3a8d1fd04d700bff35388294caf5
Julio Matias <juliocesark2@hotmail.com>
2024-11-13 05:08:50 +0000

    style: styling button listClients

--- frontend/src/components/ListClient/components/NewClientBtn/styles.ts
+++ frontend/src/components/ListClient/components/NewClientBtn/styles.ts
@@ -1,12 +1,20 @@
 import tw from 'tailwind-styled-components';
 
 export const NewClientBtn = tw.button`
- rounded
+  relative
+  overflow-hidden
+  rounded
   bg-primaryButton
- px-2
- py-1
- text-white
- hover:bg-tertiaryButton
+  px-2
+  py-1
+  text-white
+  shadow-md
+  hover:bg-tertiaryButton
+  hover:shadow-lg
   transition
+  duration-300
+  ease-in-out
   mr-9
+  focus:outline-none
+  active:scale-95
 `;
