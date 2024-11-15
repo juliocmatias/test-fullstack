35c0e4cd931f2913e99592bb9739cdf37dfa7c58
Julio Matias <juliocesark2@hotmail.com>
2024-11-15 04:49:31 +0000

    feat: implement authentication middleware and user model, update auth configuration

---
+++ backend/config/auth.ts
@@ -0,0 +1,52 @@
+import { defineConfig } from '@adonisjs/auth'
+import type { InferAuthEvents, Authenticators } from '@adonisjs/auth/types'
+import { sessionGuard, sessionUserProvider } from '@adonisjs/auth/session'
+import { jwtGuard } from '@maximemrf/adonisjs-jwt/jwt_config'
+import { JwtGuardUser, BaseJwtContent } from '@maximemrf/adonisjs-jwt/types'
+import User from '#models/user'
+
+interface JwtContent extends BaseJwtContent {
+  email: string
+}
+
+const authConfig = defineConfig({
+  // define the default authenticator to jwt
+  default: 'jwt',
+  guards: {
+    web: sessionGuard({
+      useRememberMeTokens: false,
+      provider: sessionUserProvider({
+        model: () => import('#models/user'),
+      }),
+    }),
+    // add the jwt guard
+    jwt: jwtGuard({
+      // tokenExpiresIn can be a string or a number, it can be optional
+      tokenExpiresIn: '1h',
+      // if you want to use cookies for the authentication instead of the bearer token (optional)
+      useCookies: true,
+      provider: sessionUserProvider({
+        model: () => import('#models/user'),
+      }),
+      // content is a function that takes the user and returns the content of the token, it can be optional, by default it returns only the user id
+      content: <T>(user: JwtGuardUser<T>): JwtContent => ({
+        userId: user.getId(),
+        email: (user.getOriginal() as User).email,
+      }),
+    }),
+  },
+})
+
+export default authConfig
+
+/**
+ * Inferring types from the configured auth
+ * guards.
+ */
+declare module '@adonisjs/auth/types' {
+  export interface Authenticators extends InferAuthenticators<typeof authConfig> {}
+}
+
+declare module '@adonisjs/core/types' {
+  interface EventsList extends InferAuthEvents<Authenticators> {}
+}
